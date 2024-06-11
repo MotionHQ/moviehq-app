76d3b71bb4be9d36d76d0cb26fe768aae8d3fd59
Ian D'souza <139410959+MotionHQ@users.noreply.github.com>
2024-06-11 01:40:05 -0700

    hopefully fixed problem

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Link from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div>
+      {/* <Link to="/">About</Link>
+      <Link to="/">Contact</Link> */}
+      </div>
+      <p>Created By Ian MotionHQ</p>
+    </footer>
   )
 }
 
