23a3a47eb7452afee86a32f9d576389cc1f8aab0
Ian D'souza <139410959+MotionHQ@users.noreply.github.com>
2024-06-11 01:12:18 -0700

    updated footer, but still can't see any changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className='text-center bg-dark-blue text-neon-yellow py-4'>
       <div className='flex items-center justify-center gap-4'>
         <Link to="/" >About</Link>
         <Link to="/" >Contact</Link>
       </div>
-      <p>Created By Ian D'souza</p>
+      <p className='text-sm'>Created By Ian Dsouza</p>
     </footer>
   )
 }
